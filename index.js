f19add29d6048325006f0a9b3b91e0b2b0498492
Himanshu <92928990+himanshu060301@users.noreply.github.com>
2023-09-03 12:09:33 +0530

    change3

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import usersRoute from './routes/users.js';
 import hotelsRoute from './routes/hotels.js';
 import roomsRoute from './routes/rooms.js';
 import cookieParser from 'cookie-parser';
+
 const PORT=process.env.PORT || 8800;
 
 const app=express();
@@ -46,5 +47,5 @@ app.use((err,req,res,next)=>{
 
 app.listen(PORT,()=>{
     connect()
-    console.log("Connected to port 8800.");
+    console.log("Connected to port 8800");
 });
